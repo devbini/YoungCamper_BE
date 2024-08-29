60dde49450cc719fdaded84c518d2b59dd4c9ce4
devbini <cong8685@naver.com>
2024-08-30 00:33:12 +0900

    fix: Change File 2024-08-30 파일 복사를 잘못해서 커밋을 했더라구요.. Build 확인 및 Post 요청 테스트 완료 코드입니다. 우선 하나의 세션만 존재할 수 있도록 만들어놨어요.

--- src/main/java/com/youngcamp/server/service/SessionService.java
+++ src/main/java/com/youngcamp/server/service/SessionService.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package com.youngcamp.server.service;
 
 import jakarta.servlet.http.HttpServletRequest;
@@ -8,28 +7,11 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-=======
-/*
- * **********************************
- * 파일명 : SessionService.java
- * 작성일(수정일) : 2024-08-29
- * 작성자 : 김찬빈
- * -
- * 파일 역할
- * 1. Login & Logout 로직을 담당합니다.
- * **********************************
- */
-
-package com.youngcamp.server.service;
-
-import jakarta.servlet.http.HttpSession;
->>>>>>> 9125423142438ea491d8d14359e302a8c2b0221f
 import org.springframework.stereotype.Service;
 
 @Service
 public class SessionService {
 
-<<<<<<< HEAD
   private final AuthenticationManager authenticationManager;
 
   // Security AutoManager 주입
@@ -51,20 +33,5 @@ public class SessionService {
     } else {
       throw new RuntimeException("Invalid credentials");
     }
-=======
-  // 로그인 메서드
-  public boolean login(String id, String password) {
-    boolean isSuccess = false;
-
-    // DB 조회부가 들어가야 함, 지금은 테스트용으로 root root 고정 (2024-08-29)
-    isSuccess = "root".equals(id) && "root".equals(password);
-
-    return isSuccess;
-  }
-
-  // 로그아웃 메서드
-  public void logout(HttpSession session) {
-    session.invalidate();
->>>>>>> 9125423142438ea491d8d14359e302a8c2b0221f
   }
 }
