b2e52afd87005545ff23a41463e9179e9745d933
seeun0210 <cocobell3@naver.com>
2024-09-01 13:51:11 +0900

    chore: formatter

--- src/main/java/com/youngcamp/server/config/WebConfig.java
+++ src/main/java/com/youngcamp/server/config/WebConfig.java
@@ -1,17 +1,17 @@
- package com.youngcamp.server.config;
+package com.youngcamp.server.config;
 
- import org.springframework.context.annotation.Bean;
- import org.springframework.context.annotation.Configuration;
- import org.springframework.data.web.config.EnableSpringDataWebSupport;
- import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
- import org.springframework.web.filter.ForwardedHeaderFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.data.web.config.EnableSpringDataWebSupport.PageSerializationMode;
+import org.springframework.web.filter.ForwardedHeaderFilter;
 
- @Configuration
- @EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
- public class WebConfig {
+@Configuration
+@EnableSpringDataWebSupport(pageSerializationMode = PageSerializationMode.VIA_DTO)
+public class WebConfig {
 
   @Bean
   public ForwardedHeaderFilter forwardedHeaderFilter() {
     return new ForwardedHeaderFilter();
   }
- }
+}
