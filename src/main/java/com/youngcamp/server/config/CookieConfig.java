a0be4766f9ade8fbf41dad4ac6ba4672847fb7c8
김세은 <139741006+seeun0210@users.noreply.github.com>
2024-09-03 21:48:46 +0900

    feat: logout & cookie 설정 (#20)

--- src/main/java/com/youngcamp/server/config/CookieConfig.java
+++ src/main/java/com/youngcamp/server/config/CookieConfig.java
@@ -2,11 +2,13 @@ package com.youngcamp.server.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.session.config.annotation.web.http.EnableSpringHttpSession;
 import org.springframework.session.web.http.CookieHttpSessionIdResolver;
 import org.springframework.session.web.http.DefaultCookieSerializer;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
+@EnableSpringHttpSession
 public class CookieConfig {
 
   @Bean
