c0ccc308249e79e3ff5f5c2d64a9abceadecbf3f
Limwngur <ovg07047@naver.com>
2024-09-04 20:58:56 +0900

    chore: formatter로 인한 변경사항

--- src/main/java/com/youngcamp/server/helper/AnnouncementHelper.java
+++ src/main/java/com/youngcamp/server/helper/AnnouncementHelper.java
@@ -5,17 +5,19 @@ import com.youngcamp.server.dto.AnnouncementResponse;
 
 public class AnnouncementHelper {
 
-    public static AnnouncementResponse.AnnouncementGetDetailResponse toDto(Announcement announcement) {
-        AnnouncementResponse.AnnouncementGetDetailResponse dto = AnnouncementResponse.AnnouncementGetDetailResponse.builder()
-                .title(announcement.getTitle())
-                .content(announcement.getContent())
-                .imageUrl(announcement.getImageUrl())
-                .fileUrl(announcement.getFileUrl())
-                .isPinned(announcement.getIsPinned())
-                .createdAt(String.valueOf(announcement.getCreatedAt()))
-                .updatedAt(String.valueOf(announcement.getUpdatedAt()))
-                .build();
+  public static AnnouncementResponse.AnnouncementGetDetailResponse toDto(
+      Announcement announcement) {
+    AnnouncementResponse.AnnouncementGetDetailResponse dto =
+        AnnouncementResponse.AnnouncementGetDetailResponse.builder()
+            .title(announcement.getTitle())
+            .content(announcement.getContent())
+            .imageUrl(announcement.getImageUrl())
+            .fileUrl(announcement.getFileUrl())
+            .isPinned(announcement.getIsPinned())
+            .createdAt(String.valueOf(announcement.getCreatedAt()))
+            .updatedAt(String.valueOf(announcement.getUpdatedAt()))
+            .build();
 
-        return dto;
-    }
+    return dto;
+  }
 }
