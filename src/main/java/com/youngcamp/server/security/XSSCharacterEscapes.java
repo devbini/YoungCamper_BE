f14fa104d79b9a0f3fe5754660cb9a61abd5f85c
seeun0210 <cocobell3@naver.com>
2024-09-06 00:42:20 +0900

    fix: java emoji문제 해결

--- src/main/java/com/youngcamp/server/security/XSSCharacterEscapes.java
+++ src/main/java/com/youngcamp/server/security/XSSCharacterEscapes.java
@@ -28,7 +28,14 @@ public class XSSCharacterEscapes extends CharacterEscapes {
 
   @Override
   public SerializableString getEscapeSequence(int ch) {
-    // Ensure StringEscapeUtils is properly used to escape HTML
-    return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString((char) ch)));
+    char charAt = (char) ch;
+    if (Character.isHighSurrogate(charAt) || Character.isLowSurrogate(charAt)) {
+      StringBuilder sb = new StringBuilder();
+      sb.append("\\u");
+      sb.append(String.format("%04x", ch));
+      return new SerializedString(sb.toString());
+    } else {
+      return new SerializedString(StringEscapeUtils.escapeHtml4(Character.toString(charAt)));
+    }
   }
 }
